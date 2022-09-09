74967dacdb54272a1448ea245a0578e883d928f4
mateo soto <mateo.sotoa@udea.edu.co>
2022-09-09 11:45:36 -0500

    Juego terminado

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,27 @@
+require "json"
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    @letters = ('a'..'z').to_a.sample(10)
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = params[:word].upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+end
